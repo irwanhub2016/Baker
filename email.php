68746ffdfbae17d2dce24a267943c89f55166bb9
irwan <irwansyarifudin16@gmail.com>
2018-09-21 14:51:44 +0700

    clearly

--- email.php
+++ email.php
@@ -1,14 +1,51 @@
 <?php
 
-if(isset($_POST['submit']))
-{
+#Clear Error Notif
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
+$con = mysqli_connect("localhost","id6915393_root","db2018","id6915393_mail");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+if(isset($_POST['submit']))
+
+{
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];	
 
-	echo $name.$email.$subject.$message;
+	$insert_mail= mysqli_query($con,"INSERT INTO mail (name,email,subject,message) 
+	VALUES ('$name','$email','$subject','$message')");
+
+	if($insert_mail)
+	{
+		echo '<script language="javascript">';
+		echo 'alert("Email Anda Berhasil Terkirim")';
+		echo '</script>';
+		echo '<script>';
+		echo    'window.location.href = "https://jagoanteknik.000webhostapp.com"';
+		echo '</script>';
+	}
+
+	else
+	{
+
+		echo '<script language="javascript">';
+		echo 'alert("Gagal kirim email")';
+		echo '</script>';
+		echo '<script>';
+		echo    'window.location.href = "https://jagoanteknik.000webhostapp.com"';
+		echo '</script>';
+	}
+
+	mysqli_close($con);
+
 }
 
 else
@@ -21,6 +58,8 @@ else
 
 
 
+?>
 
+<?php
 
 ?>
